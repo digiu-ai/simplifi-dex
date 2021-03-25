d1641d309efe0d901be1b2f64801534c76353176
vladimir.tidva <vladimir.tidva@messaggio.com>
2021-03-26 00:06:52 +0300

    latest bridge

--- contracts/IBridge.sol
+++ contracts/IBridge.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.8.0;
 
 interface IBridge {
-     function transmitRequest(bytes memory owner, address spender) external  returns (bytes32);
+     function transmitRequestV2(bytes memory owner, address spender) external  returns (bytes32);
 }
-    
